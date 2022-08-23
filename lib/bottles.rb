3ea39ee71d98c7c69a56227d8b900ce4f96e2fb7
Robin Wallin <walro467@gmail.com>
2022-08-23 16:09:43 +0200

    Pass 4 test

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,12 +1,23 @@
 class Bottles
   def verse(bottles)
-    "#{bottles} #{bottle_pluralization(bottles)} of beer on the wall, " +
-    "#{bottles} #{bottle_pluralization(bottles)} of beer.\n" +
-    "Take one down and pass it around, " +
-    "#{bottles - 1} #{bottle_pluralization(bottles - 1)} of beer on the wall.\n"
+    first_sentence(bottles) + second_sentence(bottles)
   end
 
   private
+
+  def first_sentence(bottles)
+    "#{bottles} #{bottle_pluralization(bottles)} of beer on the wall, " +
+    "#{bottles} #{bottle_pluralization(bottles)} of beer.\n"
+  end
+
+  def second_sentence(bottles)
+    if bottles > 1
+      "Take one down and pass it around, #{bottles - 1} " +
+      "#{bottle_pluralization(bottles -1 )} of beer on the wall.\n"
+    else
+      "Take it down and pass it around, no more bottles of beer on the wall.\n"
+    end
+  end
 
   def bottle_pluralization(bottles)
     if bottles > 1
