84dac6bf549389687ae9e7bc82aed7f91a1c6c2f
Robin Wallin <walro467@gmail.com>
2022-08-23 16:04:23 +0200

    Pass 2 test

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,8 +1,8 @@
 class Bottles
   def verse(bottles)
-    "99 bottles of beer on the wall, " +
-    "99 bottles of beer.\n" +
+    "#{bottles} bottles of beer on the wall, " +
+    "#{bottles} bottles of beer.\n" +
     "Take one down and pass it around, " +
-    "98 bottles of beer on the wall.\n"
+    "#{bottles - 1} bottles of beer on the wall.\n"
   end
 end
