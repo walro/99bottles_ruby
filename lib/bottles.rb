28a6ca1edfd9b75b999d3b98ac5c18b386b3fb56
Robin Wallin <walro467@gmail.com>
2022-08-25 16:58:49 +0200

    Convert if statements to case statement

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,16 +1,17 @@
 class Bottles
   def verse(bottles)
-    if bottles.zero?
+    case bottles
+    when 0
       "No more bottles of beer on the wall, " +
       "no more bottles of beer.\n" +
       "Go to the store and buy some more, " +
       "99 bottles of beer on the wall.\n"
-    elsif bottles == 1
+    when 1
       "1 bottle of beer on the wall, " +
       "1 bottle of beer.\n" +
       "Take it down and pass it around, " +
       "no more bottles of beer on the wall.\n"
-    elsif bottles == 2
+    when 2
       "2 bottles of beer on the wall, " +
       "2 bottles of beer.\n" +
       "Take one down and pass it around, " +
